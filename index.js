6d5770ca8cef7443a9ce15e9a9ba7a3367e11b5a
vijay-murugan <56683057+vijay-murugan@users.noreply.github.com>
2022-03-10 18:17:46 +0530

    fourth

--- index.js
+++ index.js
@@ -19,23 +19,25 @@ const corsOptions = {
   origin: "http://localhost:5000"//"https://logolego.bookmane.in"
 };
 
+
+
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-app.get('/', function(req, res) {
-  const filepath = path.resolve(__dirname,'client', 'build', 'index.html');
-  fs.readFile(filepath, 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-  res.send(result);
-  });
-});
+// app.get('/', function(req, res) {
+//   const filepath = path.resolve(__dirname,'client', 'build', 'index.html');
+//   fs.readFile(filepath, 'utf8', function (err,data) {
+//     if (err) {
+//       return console.log(err);
+//     }
+//   res.send(result);
+//   });
+// });
 
-app.use(express.static('client/build'));
+app.use(express.static('./client/build'));
 
 app.get('*', function(request, response) {
   const filePath = path.resolve(__dirname, 'client', 'build', 'index.html');
@@ -43,19 +45,19 @@ app.get('*', function(request, response) {
 });
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 app.get('/getData', cors(corsOptions), async (req, res) => {
@@ -73,22 +75,18 @@ app.use('/api/routes', routers);
 app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 app.use(bodyParser.json());
 
 
 
-// app.post("/api/uploadFile", upload.single("myFile"), (req, res) => {
-//   // Stuff to be added later
-//   console.log(req.file);
-// });
 
 
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(
-      'mongodb://localhost:27017/Logolego',
-      // "mongodb+srv://Vijay:RWHVGUd17nl3ewJe@cluster0.w5mit.mongodb.net/WhiteboardApp?retryWrites=true&w=majority",
+      // 'mongodb://localhost:27017/Logolego',
+      "mongodb+srv://Vijay:RWHVGUd17nl3ewJe@cluster0.w5mit.mongodb.net/WhiteboardApp?retryWrites=true&w=majority",
       {
         useNewUrlParser: true,
         useUnifiedTopology: true,
@@ -101,12 +99,12 @@ const connectDB = async () => {
   }
 };
 
-app.use(express.static('./client/build'))
-app.get('*', (req,res)=>{
-  res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
-})
+// app.use(express.static('./client/build'))
+// app.get('*', (req,res)=>{
+//   res.sendFile(path.resolve(__dirname,'client', 'build', 'index.html'))
+// })
 
-// connectDB()
+connectDB()
 
 app.listen(PORT, () => {
   console.log(`App listening on ${PORT}`);
